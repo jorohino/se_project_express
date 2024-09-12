49c13c65dddf3a8fe92cc05afc7c58e7066f0f09
jorohino <149121790+jorohino@users.noreply.github.com>
2024-09-12 13:58:14 -0400

    Implement findUsersByCredentials statics function in user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const isEmail = require("validator/lib/isEmail");
 
 const userSchema = new mongoose.Schema({
@@ -34,5 +35,23 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect password or email."));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect password or email."));
+        }
+
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
