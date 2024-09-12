ba3bee28fa29ac8971fa1be7a5e440ee0166ce98
jorohino <149121790+jorohino@users.noreply.github.com>
2024-09-12 14:04:28 -0400

    Add routes for sign up and sign in, remove routes from user.js

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,3 @@
 const router = require("express").Router();
-const { getUsers, createUser, getUser } = require("../controllers/users");
-
-// Return all users
-router.get("/", getUsers);
-
-// Return user by _id
-router.get("/:userId", getUser);
-
-// Create a new user
-router.post("/", createUser);
 
 module.exports = router;
