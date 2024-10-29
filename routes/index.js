517627cc19375b6bdea812152b87251744403e40
jorohino <149121790+jorohino@users.noreply.github.com>
2024-10-29 12:56:34 -0400

    Apply celebrate validation schemas

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,21 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
-const { NOT_FOUND } = require("../utils/errors");
+const NotFoundError = require("../errors/NotFoundError");
+const {
+  validateNewUser,
+  validateExistingUser,
+} = require("../middlewares/validation");
 
-router.post("/signin", login);
-router.post("/signup", createUser);
+router.post("/signin", login, validateExistingUser);
+router.post("/signup", createUser, validateNewUser);
 
 router.use("/users", userRouter);
 
 router.use("/items", itemRouter);
 
 router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
+  throw new NotFoundError("Requested resource not found");
 });
 
 module.exports = router;
