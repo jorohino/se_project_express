be48e7ec4dd24312a3bca2b33754a9a1b1ced45a
jorohino <149121790+jorohino@users.noreply.github.com>
2024-10-29 13:00:07 -0400

    Implement update user celebrate validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
+const { validateUpdateUser } = require("../middlewares/validation");
 
 // Return current user by _id
 router.get("/me", auth, getCurrentUser);
 
 // Update user name and avatar
-router.patch("/me", auth, updateUser);
+router.patch("/me", auth, updateUser, validateUpdateUser);
 
 module.exports = router;
