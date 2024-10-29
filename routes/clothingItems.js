8f54a0a6d46f2abae4e35db1bf674bef84b6a518
jorohino <149121790+jorohino@users.noreply.github.com>
2024-10-29 12:52:02 -0400

    Apply celebrate validation schemas

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -7,20 +7,21 @@ const {
   dislikeItem,
 } = require("../controllers/clothingItems");
 const auth = require("../middlewares/auth");
+const { validateNewItem, validateId } = require("../middlewares/validation");
 
 // Return all clothing items
 router.get("/", getClothingItems);
 
 // Create a new clothing item
-router.post("/", auth, createClothingItem);
+router.post("/", auth, createClothingItem, validateNewItem);
 
 // Delete a clothing item by _id
-router.delete("/:itemId", auth, deleteClothingItem);
+router.delete("/:itemId", auth, deleteClothingItem, validateId);
 
 // Like item by _id
-router.put("/:itemId/likes", auth, likeItem);
+router.put("/:itemId/likes", auth, likeItem, validateId);
 
 // Dislike item by _id
-router.delete("/:itemId/likes", auth, dislikeItem);
+router.delete("/:itemId/likes", auth, dislikeItem, validateId);
 
 module.exports = router;
